b72cefb43076ba2b6cca205107d3033197f18c32
ev3rshade <hskeney@gmail.com>
2026-03-10 20:28:46 -0400

    added instructions

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,9 @@ public class HelloWorld {
     // Your program begins with a call to main()
     public static void main(String[] args)
     {
-        // Prints "Hello, World" to the terminal window.
+        // Change this line to print something else!
+        // Person A will edit this in the main branch
+        // Person B will edit this in new-branch
         System.out.println("Hello, World");
     }
 }
